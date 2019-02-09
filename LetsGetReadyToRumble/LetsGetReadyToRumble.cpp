4cd07c8f055b936580351f5bb60bf61eb1643058
MINICOM\mrb11 <mrb113@case.edu>
2019-02-08 17:09:09 -0800

    Rumble connected xbox controller

---
+++ LetsGetReadyToRumble/LetsGetReadyToRumble.cpp
@@ -0,0 +1,53 @@
+#include "pch.h"
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+#include <Xinput.h>
+
+// Controller ID. We only expect a single XBox controller to be connected,
+// so this number will always be zero.
+#define CONTROLLER_0 0
+
+// Define magic numbers that will be easily identifiable in packet captures.
+#define LEFT_MOTOR_SPEED 0xF00D
+#define RIGHT_MOTOR_SPEED 0xBEEF
+
+int main()
+{
+    XINPUT_STATE xInputState;
+    XINPUT_VIBRATION vibration;
+
+    printf("Let's get ready to rumble!\n");
+
+    // Check if controller is connected.
+    // Program expects a single Xbox controller connected.
+    DWORD result = XInputGetState(CONTROLLER_0, &xInputState);
+
+    if (result == ERROR_SUCCESS)
+    {
+        printf("Connected\n");
+    }
+    else
+    {
+        printf("Failed to find connected controller. Error: %x\n", result);
+        goto Exit;
+    }
+
+    // Set vibration level.
+    ZeroMemory(&vibration, sizeof(XINPUT_VIBRATION));
+    vibration.wLeftMotorSpeed = LEFT_MOTOR_SPEED; // use any value between 0-65535 here
+    vibration.wRightMotorSpeed = RIGHT_MOTOR_SPEED; // use any value between 0-65535 here
+    XInputSetState(CONTROLLER_0, &vibration);
+
+    printf("Vibrating controller... press enter to stop vibration.\n");
+    getchar();
+
+    printf("Stopping vibration.\n");
+    ZeroMemory(&vibration, sizeof(XINPUT_VIBRATION));
+    vibration.wLeftMotorSpeed = 0;
+    vibration.wRightMotorSpeed = 0;
+    XInputSetState(CONTROLLER_0, &vibration);
+
+Exit:
+    return 0;
+}
